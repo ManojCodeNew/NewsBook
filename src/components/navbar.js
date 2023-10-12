eb50d22bb314e202f7de7a57c69d84da9a80ac84
manoj <manojpoojary2004@gmail.com>
2023-10-12 20:31:34 +0530

    fet-update-3

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -20,16 +20,15 @@ export default class navbar extends Component {
                     <NavLink key="sports" to='/Sports' className="p-1 mr-4 hover:text-yellow-600">Sports</NavLink >
                     <NavLink key="technology" to='/Technology' className="p-1 mr-4 hover:text-yellow-600">Technology</NavLink >
                 </div>
-
                 <div>
                     <Outlet />
                 </div>
-                <div className="relative inline-block text-left">
+                {/* <div className="relative inline-block text-left">
                     <div>
                         <button type="button" className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
                             Options
                         </button>
-                    </div>
+                    </div> */}
                     {/* <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1"> */}
                         {/* <div className="py-1" role="">
                             {/* <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" --> */}
@@ -41,7 +40,7 @@ export default class navbar extends Component {
                             {/* </form> */}
                         {/* </div> */} */
                     {/* </div> */}
-                </div>
+                {/* </div> */}
             </div>
         )
     }
